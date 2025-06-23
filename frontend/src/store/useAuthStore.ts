776f4be09fc4a9b92871d8b0bebf289b99851dae
santoschristianjeric <santoschristianjeric@gmail.com>
2025-06-24 06:44:41 +0800

    left side signup page

---
+++ frontend/src/store/useAuthStore.ts
@@ -0,0 +1,42 @@
+import { create } from 'zustand'
+import { axiosInstance } from '../lib/axios'
+
+type AuthStore = {
+    authUser: any; 
+    isSigningUp: boolean;
+    isLoggingIn: boolean;
+    isUpdatingProfile: boolean;
+    isCheckingAuth: boolean;
+    checkAuth: () => Promise<void>;
+  };
+
+export const useAuthStore = create<AuthStore>((set) => ({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+
+    checkAuth: async() => {
+        try {
+            const res = await axiosInstance.get('/auth/check');
+            console.log(res)
+
+            set({ authUser: res.data })
+        } catch (error) {
+            console.log('Error in checkAuth: ', error)
+            set({ authUser: null })
+        } finally {
+            set({ isCheckingAuth: false })
+        }
+    },
+
+    signup: async(data) => {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }
+}))
